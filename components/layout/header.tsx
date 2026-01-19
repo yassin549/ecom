5723df93ed8bde5733bdef39f09708e5890f48c4
yassin549 <khoualdiyassin26@gmail.com>
2026-01-19 17:34:41 +0100

    commit

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import Link from "next/link"
+import Image from "next/image"
 import { useState } from "react"
 import { usePathname } from "next/navigation"
 import { motion, AnimatePresence } from "framer-motion"
